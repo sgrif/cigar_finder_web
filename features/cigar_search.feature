8d410455b0b0fb90f7af43e70934811982cb0dcf
Sean Griffin <sean@seantheprogrammer.com>
2013-03-13 12:55:33 -0600

    Finding stores passing

--- features/cigar_search.feature
+++ features/cigar_search.feature
@@ -2,6 +2,6 @@ Feature: Users can search for a cigar
 
   @vcr
   Scenario: Stores near a user
-    Given I am in "Albuquerque"
+    Given I am in Albuquerque
     When I list stores near me
-    Then "Monte's Cigars, Tobacco And Gifts" should be close than "Rio Rancho Cigars"
+    Then "Monte's Cigars, Tobacco And Gifts" should be closer than "Rio Rancho Cigars"
--- features/step_definitions/cigar_search_steps.rb
+++ features/step_definitions/cigar_search_steps.rb
@@ -1,7 +1,13 @@
-Given /^I am in "([^"]*)"$/ do |location|
-  @location = Geocoder.search(location)
+class Location < Struct.new(:latitude, :longitude); end
+
+Given /^I am in Albuquerque$/ do
+  @location = Location.new(35.12384, -106.586094)
 end
 
 When /^I list stores near me$/ do
   @stores = CigarStoreSearch.stores_near(@location)
+end
+
+Then /^"(.*?)" should be closer than "(.*?)"$/ do |store, other_store|
+  @stores.index(store).should be < @stores.index(other_store)
 end
